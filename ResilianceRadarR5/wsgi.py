da1dec1f91193199ce5097d3300fdcebd91c743b
jlc1978 <james.coffey@gmail.com>
2024-09-17 06:59:28 -0400

    Added Whitenoise

--- ResilianceRadarR5/wsgi.py
+++ ResilianceRadarR5/wsgi.py
@@ -12,5 +12,5 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ResilianceRadarR5.settings')
-
+from whitenoise.base import WhiteNoise
 application = get_wsgi_application()
